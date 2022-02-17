ca124d66b230018b3ef06806470beca4156d241c
jackieyao0114 <jackie_zhiyao@lbl.gov>
2022-02-16 18:19:49 -0800

    updated bash for parameter sweep

--- Examples/Tests/Macroscopic_Maxwell/squashTime.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for timestep in $(seq 0 10 1000); do
-    newnumber='000000000'${timestep}      # get number, pack with zeros
-    newnumber=${newnumber:(-5)}       # the last seven characters
-    bash getLine.bash raw_data/${newnumber}.txt 128 | tee -a Mx_xface_center_128.txt
-done
