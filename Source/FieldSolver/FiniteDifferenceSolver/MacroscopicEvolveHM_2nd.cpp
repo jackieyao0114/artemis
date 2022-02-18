4e34dacec7a2f6d4e9020de4d1f31d8627a82bf3
jackieyao0114 <jackie_zhiyao@lbl.gov>
2022-02-18 11:45:25 -0800

    hacked in dirichlet bc to the synced code

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveHM_2nd.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveHM_2nd.cpp
@@ -867,6 +867,14 @@ void FiniteDifferenceSolver::MacroscopicEvolveHMCartesian_2nd(
                             }
                         }
 
+                        // Hack in Dirichlet BC
+                        if (k == 64){
+                            //amrex::Abort("code gets here");
+                            M_zface(i, j, k, 0) = 0;
+                            M_zface(i, j, k, 1) = mag_Ms_zface_arr(i, j, k+1); // pin M to be in y direction
+                            M_zface(i, j, k, 2) = 0;
+                        }
+
                         // calculate M_error_zface
                         // x,y,z component on z-faces of grid
                         for (int icomp = 0; icomp < 3; ++icomp) {
